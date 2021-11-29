d5bed40d9c21812e9656ae0ab0c7e67f80855f7d
AquiLiusOP <adibmannanrafi2003@gmail.com>
2021-11-30 00:33:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const os = require('os')
+console.log('bit', os.arch())
+
+const app = express()
+const port = process.env.PORT || 6969;
+
+app.use(cors());
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3uvbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("onlineShop");
+        const productCollection = database.collection("products");
+        const orderCollection = database.collection("order");
+
+        //  GET POST 
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            const count = await cursor.count();
+            let products;
+            if (page) {
+                products = await cursor.skip(page * size).limit(size).toArray()
+            }
+            else (
+                products = await cursor.toArray()
+            )
+            res.send({ count, products })
+        });
+
+        // GET POST
+        app.post('/products/byKey', async (req, res) => {
+            const keys = req.body;
+            const query = { key: { $in: keys } }
+            const products = await productCollection.find(query).toArray();
+            res.json(products)
+
+        })
+        // ADD ORDER API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
